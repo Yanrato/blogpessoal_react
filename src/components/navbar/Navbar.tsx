a9ad8e4779073eaf35262533b7ab0a9ee4460fde
Yanrato <yanferreira985@gmail.com>
2026-08-18 09:00:45 -0300

    💄 UI:  Criação de rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,19 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
             <div className="flex justify-center bg-indigo-900 py-4 w-full bg-amber-700 text-white">
 
                 <div className="container flex justify-between text-lg mx-8" >
-                    Blog Pessoal
+                    <Link to="/" className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className="flex gap-4">
                         Postagens
                         Temas
-                        Cadastrar
-                        tema
+                        Cadastrar tema
                         Perfil
-                        Sair
+                       <Link to="/login" className="hover:underline"> Sair</Link>
                     </div>
                 </div>
             </div>
---
+++ src/pages/cadastro/Cadastro.tsx
@@ -0,0 +1,12 @@
+import Footer from "../../components/footer/Footer";
+
+function Cadastro() {
+  return (
+    <>
+    <div>Cadastro</div>
+    <Footer/>
+    </>
+  )
+}
+
+export default Cadastro
